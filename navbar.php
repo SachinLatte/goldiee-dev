9013cc028784323f4b9e5382fa869a38f75150a2
SachinLatte <sachin@digitallatte.in>
2024-12-23 12:33:16 +0530

    our story page in the working

--- navbar.php
+++ navbar.php
@@ -1,21 +1,21 @@
 <div class="navigation-main">
   <div class="custom-container">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="javascript:void(0)"><img src="assets/img/logo.webp" alt="Brand Logo"></a>
+      <a class="navbar-brand" href="index.php"><img src="assets/img/logo.webp" alt="Brand Logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="javascript:void(0)">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               About Us
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="javascript:void(0)">Our Story</a>
+              <a class="dropdown-item" href="our-story.php">Our Story</a>
               <a class="dropdown-item" href="javascript:void(0)">Legacy & Leadership</a>
               <a class="dropdown-item" href="javascript:void(0)">Mission / Vision</a>
               <a class="dropdown-item" href="javascript:void(0)">Milestones</a>
--- our-story.php
+++ our-story.php
@@ -1,8 +1,96 @@
 <?php include 'head.php' ?>
 <section class="our-story-head-section">
 <?php include 'navbar.php' ?>
-
 </section>
 <!--our story head section end-->
+<div class="top-breadcrumb">
+    <div class="custom-container">
+        <div class="bread-crumb">
+        <ul class="clearfix">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="javascript:void(0)">About Us</a></li>
+            <li><a href="javascript:void(0)" class="active">Our Story</a></li>
+        </ul>
+        </div>
+    </div>
+</div>
+<!--bread crumb end-->
+  <section class="our-story-div">
+    <div class="section-full">
+                <div class="container-fluid">
+                    <div class="section-content">
+                    	<div class="row">
+                        	<div class="col-lg-6 col-md-12 bg-cover wt-half-left-outer">
+                        		<div class="wt-half-left">
+                                    <div class="wt-media">
+                                        <img src="assets/img/machine.webp" alt="">
+                                    </div>
+                                </div> 
+                            </div>
+                            <div class="col-lg-6 col-md-12 wt-half-right-outer mt-5">
+                    			<div class="wt-half-right p-a50 p-l50 p-r0">
+                                    <!-- TITLE START -->
+                                    <div class="section-head text-left">
+                                        <h4 class="site-text-primary text-uppercase wt-title-small">Our specialization</h4>
+                                        <h2 class="mt-3  mb-2">Helping you and your house become better acquainted.</h2>
+                                        <p>Fited his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly.</p>
+                                    </div>
+                                    <!-- TITLE END -->                              
+                                     <div class="row">
+                                        <div class="col-md-4 col-sm-4">
+                                            <div class="m-b30 text-center wt-icon-box-wraper center">
+                                                <div class="project-counter font-40 font-weight-600 m-b5  site-text-primary">250</div>
+                                                <h4>PROJECT COMPLETED</h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-sm-4">
+                                            <div class="m-b30 text-center wt-icon-box-wraper center">
+                                                <div class="project-counter font-40 font-weight-600 m-b5 site-text-primary">300</div>
+                                                <h4>HAPPY CLIENTS</h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-sm-4">
+                                            <div class="m-b30 text-center wt-icon-box-wraper center">
+                                                <div class="project-counter font-40 font-weight-600 m-b5  site-text-primary">750</div>
+                                                <h4>WORKERS EMPLOYED</h4>
+                                            </div>
+                                        </div>                            
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+        </div>
+  </section>
+<!--our story end-->
+<section class="our-story-vision">
+    <div class="custom-container">
+        <div class="our-story-vision-box">
+            <h1>Our History</h1>
+            <img src="assets/img/divider.png" alt="divider img" class="mt-4 mb-4 divider-img">
+            <h2 class="text-center">Over <span>45</span> Years of <br> a grand vision for india</h2>
+            <img src="assets/img/divider.png" alt="divider img" class="mt-4 mb-4 divider-img">
+            <p class="text-center">As part of diversification into the processed food business, "One One" brand <br> was launched by Goldiee Group. Noodles, Pasta and Macaroni <br> are the products under the processed food brand. 
+            </p>
+            <div class="story-founder-box">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="story-founder-info">
+                            <h1 class="mt-5">Alexandra <br> H. Elizabeth</h1>
+                            <p class="mt-4">Founder-Chairman, <br>
+                            Lorem Industries Limited</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="assets/img/dhirubhai-ambani.png" alt="founder img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--our story vision end-->
 <?php include 'footer.php'?>
 <?php include 'tail.php' ?>
